000a7a822ab05d91be49f0e9c0d859510db87717
jwalith <jwalithkristam@gmail.com>
2024-05-03 01:44:59 -0400

    Add files via upload

--- R/autoPreScreenPredictors.R
+++ R/autoPreScreenPredictors.R
@@ -1,4 +1,4 @@
-autoPreScreenPredictors <- function(X, y, k=10) {
+autoPreScreenPredictors <- function(X, y, k = 10) {
   # Ensure k is a valid number
   if (k > ncol(X)) {
     warning(paste("k is greater than the number of columns in X. Adjusting k to", ncol(X)))
@@ -19,7 +19,6 @@ autoPreScreenPredictors <- function(X, y, k=10) {
     stop("Data became empty after removing missing values. Please check your data.")
   }
   
-  
   # Create a data frame to store the test results
   test_results <- data.frame(
     predictor = colnames(X),
@@ -36,26 +35,38 @@ autoPreScreenPredictors <- function(X, y, k=10) {
     predictor = X[[i]]
     predictor_name = colnames(X)[i]
     
-    if (is.numeric(predictor) && is_y_numeric && length(unique(y)) != 2 && length(unique(predictor)) != 2) {
+    if (is.numeric(predictor) && is_y_numeric) {
       # Continuous predictor with continuous response - Pearson Correlation
       test = cor.test(predictor, y)
       test_results$p_value[i] = test$p.value
       test_results$method[i] = "Pearson Correlation"
-    } else if ((is.factor(predictor) || is.character(predictor)) && is_y_numeric) {
-      # Categorical predictor with continuous response - ANOVA
-      test = aov(y ~ predictor)
-      test_results$p_value[i] = summary(test)[[1]]$`Pr(>F)`[1]
-      test_results$method[i] = "ANOVA"
-    } else if ((is.factor(predictor) || is.character(predictor)) && !is_y_numeric) {
-      # Categorical predictor with categorical response - Chi-squared Test
-      test = chisq.test(table(predictor, y))
-      test_results$p_value[i] = test$p.value
-      test_results$method[i] = "Chi-squared Test"
-    } else if ((is.numeric(predictor) && length(unique(y)) == 2)  || (is.numeric(y) && length(unique(predictor)) == 2)) {
-      # Continuous predictor with binary response - T-test
-      test = t.test(predictor ~ y)
-      test_results$p_value[i] = test$p.value
-      test_results$method[i] = "T-test"
+    } else if (is.factor(predictor) || is.character(predictor)) {
+      if (is_y_numeric) {
+        # Categorical predictor with continuous response - ANOVA
+        test = aov(y ~ predictor)
+        test_results$p_value[i] = summary(test)[[1]]$`Pr(>F)`[1]
+        test_results$method[i] = "ANOVA"
+      } else {
+        # Categorical predictor with categorical response - Chi-squared Test
+        test = chisq.test(table(predictor, y))
+        test_results$p_value[i] = test$p.value
+        test_results$method[i] = "Chi-squared Test"
+      }
+    } else if ((is.numeric(predictor) && !is_y_numeric) || 
+               (is_y_numeric && length(unique(predictor)) == 2)) {
+      # T-test - ensure the response or predictor is binary
+      if (is.numeric(y) && length(unique(y)) == 2) {
+        # Continuous predictor with binary response
+        test = t.test(predictor ~ y)
+        test_results$p_value[i] = test$p.value
+        test_results$method[i] = "T-test"
+      } else if (is.numeric(predictor) && is.factor(y) && 
+                 length(unique(y)) == 2) {
+        # Binary predictor with continuous response
+        test = t.test(predictor ~ y)
+        test_results$p_value[i] = test$p.value
+        test_results$method[i] = "T-test"
+      }
     }
   }
   
